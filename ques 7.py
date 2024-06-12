e79d9d2f64e1cac13823c43ded98c6eba7e07390
divya795795 <123897301+divya795795@users.noreply.github.com>
2024-06-12 20:46:32 +0530

    ques 7

---
+++ ques 7.py
@@ -0,0 +1,8 @@
+# Ask the user for a string input
+user_input = input("Please enter a string: ")
+
+# Calculate the length of the input string
+length_of_string = len(user_input)
+
+# Print the length of the string
+print(f"The length of the entered string is: {length_of_string}")
