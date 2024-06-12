578201419d5e4fd9bfa0f4704c0712b1ff48bc89
divya795795 <123897301+divya795795@users.noreply.github.com>
2024-06-12 22:43:06 +0530

    ques 27

---
+++ ques 27.py
@@ -0,0 +1,8 @@
+# Ask the user for a string input
+user_input = input("Enter a string: ")
+
+# Convert the string to a list of its characters
+char_list = list(user_input)
+
+# Print the list of characters
+print(f"The list of characters is: {char_list}")
