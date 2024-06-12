5c32a5ab0a2f7ef36a925d3bb6847fd654efd07c
divya795795 <123897301+divya795795@users.noreply.github.com>
2024-06-12 20:48:46 +0530

    ques 10

---
+++ ques 10.py
@@ -0,0 +1,8 @@
+# Ask the user for a string input
+user_input = input("Please enter a string: ")
+
+# Convert the string to uppercase
+uppercase_string = user_input.upper()
+
+# Print the uppercase string
+print(f"The uppercase string is: {uppercase_string}")
