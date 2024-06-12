b3d0cb260e85f59989a9936d3c478913fde0c78d
divya795795 <123897301+divya795795@users.noreply.github.com>
2024-06-12 22:35:43 +0530

    ques 17

---
+++ ques 17.py
@@ -0,0 +1,8 @@
+# Ask the user for a string input
+user_input = input("Enter a string: ")
+
+# Convert the string to title case
+title_case_string = user_input.title()
+
+# Print the title case string
+print(f"The title case string is: {title_case_string}")
